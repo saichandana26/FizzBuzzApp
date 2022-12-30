f7c3652727ed837f6c99e3c33418ad6158359799
saichandana.pagadala <saichandana.pagadala@outlook.com>
2022-12-30 18:50:25 +0530

    Add project files.

---
+++ FizzBuzzApp/Controllers/HomeController.cs
@@ -0,0 +1,75 @@
+﻿using FizzBuzzApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FizzBuzzApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            FizzBuzzNumber fizzBuzzNumber = new FizzBuzzNumber();
+            fizzBuzzNumber.DayOfWeek = DateTime.Today.DayOfWeek.ToString();
+            
+            fizzBuzzNumber.DisplayNumbers = null;
+            return View(fizzBuzzNumber);
+        }
+        [HttpPost]
+        public IActionResult Index(FizzBuzzNumber fizzBuzzNumber)
+        {
+            if (fizzBuzzNumber.DayOfWeek == "wednesday")
+            {
+                fizzBuzzNumber.ThreeDivisible = "Wizz";
+                fizzBuzzNumber.FiveDivisible = "Wuzz";
+                fizzBuzzNumber.ThreeAndFiveDivisible = "WizzWuzz";
+            }
+            else
+            {
+                fizzBuzzNumber.ThreeDivisible = "Fizz";
+                fizzBuzzNumber.FiveDivisible = "Buzz";
+                fizzBuzzNumber.ThreeAndFiveDivisible = "FizzBuzz";
+            }
+
+            fizzBuzzNumber.DisplayNumbers = new List<string>(fizzBuzzNumber.Number);
+            for (int i = 1; i <= fizzBuzzNumber.Number; i++)
+            {
+                if (i % 3 == 0 & i % 5 == 0)
+                {
+                    fizzBuzzNumber.DisplayNumbers.Add(fizzBuzzNumber.ThreeAndFiveDivisible);
+                }
+                else if (i % 3 == 0)
+                {
+                    fizzBuzzNumber.DisplayNumbers.Add(fizzBuzzNumber.ThreeDivisible);
+                }
+                else if (i % 5 == 0)
+                {
+                    fizzBuzzNumber.DisplayNumbers.Add(fizzBuzzNumber.FiveDivisible);
+
+                }
+                else
+                {
+                    fizzBuzzNumber.DisplayNumbers.Add(i.ToString());
+                }
+            }
+            return View(fizzBuzzNumber);
+        }
+
+        [HttpGet]
+        public IActionResult Sai()
+        {
+            FizzBuzzNumber fizzBuzzNumber1 = new FizzBuzzNumber();
+            fizzBuzzNumber1.Number = 100;
+            return View(fizzBuzzNumber1);
+        }
+
+        [HttpPost]
+        public IActionResult Sai(FizzBuzzNumber fizzBuzzNumber)
+        {
+
+            return View(fizzBuzzNumber);
+        }
+
+
+    }
+}
+ 
---
+++ FizzBuzzApp/Models/FizzBuzzNumber.cs
@@ -0,0 +1,16 @@
+﻿namespace FizzBuzzApp.Models
+{
+    public class FizzBuzzNumber
+    {
+        public int Number { get; set; }
+        public List<string>? DisplayNumbers { get; set; }
+
+        public string? DayOfWeek { get; set; }
+
+        public string? ThreeDivisible { get; set; }
+        public string? FiveDivisible { get; set; }
+
+        public string? ThreeAndFiveDivisible { get; set; }
+
+    }
+}
